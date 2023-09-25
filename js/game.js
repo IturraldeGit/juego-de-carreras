5e2f02a6ee3969b30a884afc48c5e0b2324004e5
Daniel Iturralde <daniel_iturralde_b@hotmail.com>
2023-09-25 16:02:32 -0500

    Print Gamemap with JS

---
+++ js/game.js
@@ -0,0 +1,41 @@
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+let canvasSize;
+let elementsSize;
+
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+function setCanvasSize() {
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8;
+    } else {
+        canvasSize = window.innerHeight * 0.8;
+    }
+
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    elementsSize = canvasSize / 10;
+
+    startGame();
+}
+
+function startGame() {
+    game.font = (elementsSize - 5) + 'px Verdana';
+    game.textAlign = 'end';
+
+    const map = maps[0];
+    const mapRows = map.trim().split('\n');
+    const mapRowCols = mapRows.map(row => row.trim().split(''));
+
+    mapRowCols.forEach((row, rowI) => {
+        row.forEach((col, colI) => {
+            const emoji = emojis[col];
+            const posX = elementsSize * (colI + 1 + 0.15);
+            const posY = elementsSize * (rowI + 1 - 0.19);
+            game.fillText(emoji, posX, posY);
+        });
+    })
+}       
